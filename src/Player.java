523ea350d5b122e94774e040c26d4fa458f4b9f4
Aaron Koeppe <aaronkoeppe@NB-DAFRITZ1-1.ad.wsc.edu>
2022-02-11 13:53:03 -0600

    player

---
+++ src/Player.java
@@ -0,0 +1,24 @@
+public class Player {
+    private String teamName;
+    private String name;
+    private int number;
+    private int onePointer;
+    private int twoPointer;
+    private int threePointer;
+    private int fouls;
+    private int techFouls;
+    private int attempts;
+
+    public Player (){
+     this.teamName = null;
+     this.name = null;
+     this.number = 0;
+     this.onePointer = 0;
+     this.twoPointer = 0;
+     this.threePointer = 0;
+     this.fouls = 0;
+     this.attempts = 0;
+
+    }
+}
+
